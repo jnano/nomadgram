1aa4a83053fe2090e27f93752f44841c84187a6a
hunskang <hunskang@hunskangui-MacBook-Pro.local>
2018-08-02 16:37:13 +0900

    #1-22 Created the Like model and fixing Comment and Image models

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -13,17 +13,27 @@ class TimeStempedModel(models.Model):
 
 class Image(TimeStempedModel):
 
+    """ Image Model """
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
-
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
 
 
 class Comment(TimeStempedModel):
 
+    """ Comment Model """
+
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
 
 
 
 class Like(TimeStempedModel):
-    creator = models.ForeignKey(user_models.User)
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, on_delete = models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
