c2b70e6b871a8738b594011a18341ab8372277d1
hunskang <hunskang@hunskangui-MacBook-Pro.local>
2018-08-02 12:16:23 +0900

    #1-21 Explaining Model Relationships

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStempedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+
+class Image(TimeStempedModel):
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+
+
+
+class Comment(TimeStempedModel):
+
+    message = models.TextField()
