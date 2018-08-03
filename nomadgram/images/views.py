e12dd27fc6a8d6f77e4e56308a42ad08fef98a7d
hunskang <hunskang@hunskangui-MacBook-Pro.local>
2018-08-03 15:42:34 +0900

    #1-32 Creating the URLS and testing the Image Serializers

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -2,12 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import models, serializers
 
-
 class ListAllImages(APIView):
     
     def get(self, request, format=None):
 
-        all_images = models.Image.object.all()
+        all_images = models.Image.objects.all()
 
         serializer = serializers.ImageSerializer(all_images, many=True)
 
